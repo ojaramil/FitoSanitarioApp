9e4364d6de3e7f018ab29cc34b65b3c84b7b2bc1
Orlando Jaramillo <tu-email@ejemplo.com>
2026-02-04 13:40:02 -0500

    chore: habilitar config.js en GitHub (seguro por restricciones HTTP)

---
+++ config.js
@@ -0,0 +1,6 @@
+// Archivo de configuración para API Keys
+// NO COMPARTIR ESTE ARCHIVO - Solo para uso local
+
+window.APP_CONFIG = {
+    GEMINI_API_KEY: "AIzaSyC2mfukzzmBfxnEYsLpLtuhat6PpsMBkQI"
+};
